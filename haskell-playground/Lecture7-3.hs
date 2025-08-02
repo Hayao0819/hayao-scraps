089d45ee36bc4d0e1ccf2fc05633092c7fca334a
hayao <shun819.mail@gmail.com>
2025-08-03 01:38:33 +0900

    delete: remove unused Haskell lecture files

--- haskell-playground/Lecture7-3.hs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env runghc
-
-myrev :: [a] -> [a]
-myrev [] = []
-myrev arr@(top : other) = myrev other ++ [top]
-
-main = print (myrev [1, 2, 3])
--- haskell-playground/Lecture8-2.hs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env runghc
-
-isUpper :: Char -> Bool
-isUpper = (`elem` ['A' .. 'Z'])
-
-main = do
-    print (isUpper 'A')
-    print (isUpper 'n')
